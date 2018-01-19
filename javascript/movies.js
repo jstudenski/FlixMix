bc9ab16cb9029210072a594d430ec56c2f852951
mikelsito <mikelsito@gmail.com>
2018-01-18 21:21:31 -0600

    Updated genres object, added css styling.

--- javascript/movies.js
+++ javascript/movies.js
@@ -7,84 +7,90 @@ $(document).ready(function() {
      {
         "emoji" : "images/moods/action.png",
         "id": 28,
-        "name": "Action"
+        "name": "action"
+      },
+      {
+        "emoji" : "images/moods/adventure.png",
+        "id": 12,
+        "name": "adventure"
       },
       // {
-      //   "id": 12,
-      //   "name": "Adventure"
-      // },
-      // {
       //   "id": 16,
-      //   "name": "Animation"
+      //   "name": "animation"
       // },
       {
         "emoji" : "images/moods/comedy.png",
         "id": 35,
-        "name": "Comedy"
+        "name": "comedy"
       },
       // {
       //   "id": 80,
-      //   "name": "Crime"
+      //   "name": "crime"
       // },
-      // {
-      //   "id": 99,
-      //   "name": "Documentary"
-      // },
-      // {
-      //   "id": 18,
-      //   "name": "Drama"
-      // },
+      {
+        "emoji" : "images/moods/documentary.png",
+        "id": 99,
+        "name": "documentary"
+      },
+      {
+        "emoji" : "images/moods/drama.png",
+        "id": 18,
+        "name": "drama"
+      },
       {
         "emoji" : "images/moods/family.png",
         "id": 10751,
-        "name": "Family"
+        "name": "family"
       },
-      // {
-      //   "id": 14,
-      //   "name": "Fantasy"
-      // },
-      // {
-      //   "id": 36,
-      //   "name": "History"
-      // },
+      {
+        "emoji" : "images/moods/fantasy.png",
+        "id": 14,
+        "name": "fantasy"
+      },
+      {
+        "emoji" : "images/moods/history.png",
+        "id": 36,
+        "name": "history"
+      },
       {
         "emoji" : "images/moods/horror.png",
         "id": 27,
-        "name": "Horror"
+        "name": "horror"
       },
-      // {
-      //   "id": 10402,
-      //   "name": "Music"
-      // },
-      // {
-      //   "id": 9648,
-      //   "name": "Mystery"
-      // },
+      {
+        "emoji" : "images/moods/music.png",
+        "id": 10402,
+        "name": "music"
+      },
+      {
+        "emoji" : "images/moods/mystery.png",
+        "id": 9648,
+        "name": "mystery"
+      },
       {
         "emoji" : "images/moods/romance.png",
         "id": 10749,
-        "name": "Romance"
+        "name": "romance"
+      },
+      {
+        "emoji" : "images/moods/scifi.png",
+        "id": 878,
+        "name": "scifi"
       },
       // {
-      //   "id": 878,
-      //   "name": "Science Fiction"
-      // },
-      // {
-      //   "id": 10770,
-      //   "name": "TV Movie"
-      // },
-      // {
       //   "id": 53,
-      //   "name": "Thriller"
+      //   "name": "thriller"
       // },
-      // {
-      //   "id": 10752,
-      //   "name": "War"
-      // },
-      // {
-      //   "id": 37,
-      //   "name": "Western"
-      // }
+      {
+        "emoji" : "images/moods/war.png",
+        "id": 10752,
+        "name": "war"
+      },
+      {
+        "emoji" : "images/moods/western.png",
+        "id": 37,
+        "name": "western"
+      }
     ]
   }
 
@@ -108,10 +114,6 @@ $(document).ready(function() {
 
   renderButtons();
 
-  function getMovie(movieGenre) {
-
-  }
-
   $('.emoji-button').on('click', function(){
 
         var movieGenre = $(this).attr("genre-id");
@@ -127,11 +129,6 @@ $(document).ready(function() {
   });
 
 });
-
-    //     var genreId = $(this).attr("genre-id");
-
-    //     console.log(genreId);
-
     //     var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=0531a8a6b116e7a8d8b9559e7b0b1416&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genreId
 
     //     $.ajax({
