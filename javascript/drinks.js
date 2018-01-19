33640566c22bb42acd9f8e8fd36553a72f7bcb61
jstudenski <jstudenski@gmail.com>
2018-01-18 19:55:00 -0600

    added comments to drinks.js

--- javascript/drinks.js
+++ javascript/drinks.js
@@ -25,18 +25,14 @@ var drinks = [
 var drink = {};
 // get random drink
 function getDrink(genre) {
-
+  // find random drink from array
   var random = drinks[Math.floor(Math.random()*drinks.length)];
-
+  // see if the genre matches
   var index = random.genre.indexOf(genre);
-
   if (index === -1) {
-    console.log("incorrect");
-    getDrink(genre);
+    getDrink(genre); // start over
   } else {
-    drink = random;
-    console.log(random);
+    drink = random; // drink found!
   }
-
 }
 
